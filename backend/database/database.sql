3ae962c032c7fea311b57bd988b26daa4b27ab5b
justdheja <justdheja@gmail.com>
2021-10-02 19:21:48 +0700

    add data

--- backend/database/database.sql
+++ backend/database/database.sql
@@ -10,8 +10,20 @@ INSERT INTO users (user_name, balance, membership) VALUES('Uddin', 50000, 0);
 
 INSERT INTO users (user_name, balance, membership) VALUES('Blangkon', 100000, 0);
 
+INSERT INTO users (user_name, balance, membership) VALUES('John Doe', 1000000, 1);
+
+INSERT INTO users (user_name, balance, membership) VALUES('Jane Doe', 50, 2);
+
+INSERT INTO users (user_name, balance, membership) VALUES('Andy Smith', 1000, 0);
+
 INSERT INTO banners (banner_name, banner_image, next_url, date_start, date_end) VALUES('Diskon Makanan', 'diskon_makanan.png', 'next.com', '20211002', '20211130');
 
 INSERT INTO banners (banner_name, banner_image, next_url, date_start, date_end) VALUES('Diskon Gadget', 'diskon_gadget.png', 'next.com', '20211002', '20211130');
 
 INSERT INTO banners (banner_name, banner_image, next_url, date_start, date_end) VALUES('Diskon Baju', 'diskon_baju.png', 'next.com', '20211002', '20211130');
+
+INSERT INTO banners (banner_name, banner_image, next_url, date_start, date_end) VALUES('Upgrade Membership', 'upgrade_membership.png', 'https://tokopedia.com', '20211003', '20211230');
+
+INSERT INTO banners (banner_name, banner_image, next_url, date_start, date_end) VALUES('Top Up Saldo Tokopedia', 'topup_saldo.png', 'https://tokopedia.com', '20211003', '20211103');
+
+INSERT INTO banners (banner_name, banner_image, next_url, date_start, date_end) VALUES('Kupon WIB', 'kupon_wib.png', 'https://tokopedia.com', '20211003', '20211103');
