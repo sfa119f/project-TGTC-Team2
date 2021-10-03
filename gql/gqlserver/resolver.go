0350f9ccb18a02ee31719af9156955dce7972dd3
Syarifuddin <syarifuddinfa@gmail.com>
2021-10-03 11:45:42 +0700

    getXbanner gql

--- gql/gqlserver/resolver.go
+++ gql/gqlserver/resolver.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"tgtc/backend/dictionary"
+	"strconv"
 
 	"github.com/graphql-go/graphql"
 )
@@ -42,6 +43,27 @@ func (r *Resolver) GetUser() graphql.FieldResolveFn {
 func (r *Resolver) GetAllProducts() graphql.FieldResolveFn {
 	return func(p graphql.ResolveParams) (interface{}, error) {
 		return nil, nil
+	}
+}
+
+func (r *Resolver) GetAllBannerOfUser() graphql.FieldResolveFn {
+	return func(p graphql.ResolveParams) (interface{}, error) {
+		user_id, _ := p.Args["user_id"].(int)
+		resp, err := http.Get(r.APIEndpoint + "/userXbanners/?userId=" + strconv.Itoa(user_id))
+
+		if err != nil {
+			fmt.Println("err api call getuser:", err)
+		}
+		body, err := ioutil.ReadAll(resp.Body)
+
+		userResp := struct {
+			Data dictionary.User
+			Error string
+		}{}
+		respJsonString := userResp
+		json.Unmarshal(body, &respJsonString)
+
+		return respJsonString.Data, err
 	}
 }
 
